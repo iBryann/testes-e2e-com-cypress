3e066f09b7f40775dbc28263f12afaa591822aec
Bryann Henrique <bryann.enrique@gmail.com>
2023-07-27 11:30:31 -0300

    pmais_login spec in progress

--- cypress/e2e/pmais_login.cy.js
+++ cypress/e2e/pmais_login.cy.js
@@ -1,8 +1,8 @@
 describe('Poliedro Tests', () => {
 
-  it('P+ login', () => {
+  it('BQ: Export document', () => {
+    cy.log('P+ login');
     cy.intercept('POST', 'https://pmais-api.p4ed.com/api/v1/login').as('postLogin');
-
     cy.visit('https://pmais.p4ed.com/');
     cy.get('input[name="email"]').type(Cypress.env('PMAIS_USERNAME'));
     cy.get('input[name="password"]').type(Cypress.env('PMAIS_PASSWORD'));
@@ -10,6 +10,7 @@ describe('Poliedro Tests', () => {
     cy.wait('@postLogin');
     cy.contains('p', 'Aplicativos').should('be.visible');
 
+    cy.log('Select profile');
     cy.visit('https://pmais.p4ed.com/poliedro/users');
     cy.get('#combo-box-escola').type('2005', {
       waitForAnimations: true,
@@ -20,6 +21,9 @@ describe('Poliedro Tests', () => {
       .type(`${'{downArrow}'.repeat(4)}{enter}{esc}`);
     cy.contains('div', 'Professor 02').click();
     cy.contains('button', 'Logar como usuário').click();
+
+    cy.log('Open BQ');
     cy.contains('button', 'Banco de Questões').click();
   });
+
 });
